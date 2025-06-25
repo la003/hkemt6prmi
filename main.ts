54218e7f847fb9acb0efc1fc20354a9c6aa2866b
la003 <147426667+la003@users.noreply.github.com>
2025-06-25 09:17:55 +0200

    Erste Dateien für MakeCode-Projekt

---
+++ main.ts
@@ -0,0 +1,35 @@
+//% color="#0000FF"
+namespace Mindstorms {
+
+    export enum ServoAdresse {
+        //% block="Servo_1"
+        Servo1 = 0x0B,
+        //% block="Servo_2"
+        Servo2 = 0x1B,
+        //% block="Servo_3"
+        Servo3 = 0x2B,
+        //% block="Servo_4"
+        Servo4 = 0x3B,
+        //% block="Servo_5"
+        Servo5 = 0x4B,
+        //% block="Servo_6"
+        Servo6 = 0x5B,
+        //% block="Servo_7"
+        Servo7 = 0x6B,
+        //% block="Servo_8"
+        Servo8 = 0x7B,
+    }
+
+    //% block="Bewege %adresse auf %position Grad"
+    //% adresse.defl=ServoAdresse.Servo1
+    //% position.min=0 position.max=180
+    export function servoBewegen(adresse: ServoAdresse, position: number) {
+        // Buffer mit 2 Bytes: [Adresse, Position]
+        let buffer = pins.createBuffer(2)
+        buffer[0] = adresse
+        buffer[1] = position
+
+        // Senden über I²C
+        pins.i2cWriteBuffer(buffer)
+    }
+}
